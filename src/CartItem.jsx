a126fa494bfbe88699892138c8afec2f9f1ec5b6
PatriciaSugapong <patriciaannesugapong@gmail.com>
2024-09-20 15:15:32 -0400

    added add to cart button

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -9,27 +9,38 @@ const CartItem = ({ onContinueShopping }) => {
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
- 
+    const totalAmount = cart.reduce((total, item) => {
+        return total + (item.quantity * item.cost);
+    }, 0);
+    
+    return totalAmount.toFixed(2);
   };
 
   const handleContinueShopping = (e) => {
-   
+    onContinueShopping();
+  };
+  
+  const handleCheckoutShopping = (e) => {
+    alert('Functionality to be added for future reference');
   };
 
-
-
   const handleIncrement = (item) => {
+    dispatch(updateQuantity({ name: item.name, quantity: item.quantity + 1}))
   };
 
   const handleDecrement = (item) => {
-   
+    if (item.quantity > 1){
+           dispatch(updateQuantity({ name: item.name, quantity: item.quantity - 1}))
+    }
   };
 
   const handleRemove = (item) => {
+    dispatch(removeItem(item.name))
   };
 
   // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
+    return (item.quantity * item.cost).toFixed(2);
   };
 
   return (
--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -289,6 +289,7 @@ const handlePlantsClick = (e) => {
                                 <div className='product-title'>{plant.name}</div>
                                 <div className='product-description'>{plant.description}</div>
                                 <div className='product-description'>{plant.cost}</div>
+                                <button className='product-button' onClick={() => handleAddToCart(plant)}>Add to Cart</button>
                             </div>
                         ))}
                     </div>
